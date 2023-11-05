0f742bd6241ad3a663637f64eab8f856bc45407a
Sophia_Yu <35753770+xiangpingyu@users.noreply.github.com>
2023-11-05 11:14:17 +0800

    Add files via upload

---
+++ Analysis/02/2_3qnew_reordered_s.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# 读取原始数据文件，假设数据文件名为qnew.csv
+input_file = 'qnew.csv'
+df = pd.read_csv(input_file)  # 假设数据文件是以制表符分隔的
+
+# 定义一个函数，用于处理每个单元格，确保中间没有空格
+def process_cell(cell):
+    if pd.notna(cell) and isinstance(cell, str):
+        return ' '.join(cell.split())  # 移除中间的空格
+    return cell
+
+# 重新排列列的顺序
+desired_columns = ['seq_name', 'sstart_b41', 'send_b41', 'sstart_b31', 'send_b31',
+                   'sstart_b42', 'send_b42', 'sstart_b21', 'send_b21',
+                   'sstart_b43', 'send_b43', 'sstart_b32', 'send_b32',
+                   'sstart_b44', 'send_b44', 'sstart_b11', 'send_b11',
+                   'sstart_b45', 'send_b45', 'sstart_b33', 'send_b33',
+                   'sstart_b46', 'send_b46', 'sstart_b22', 'send_b22',
+                   'sstart_b47', 'send_b47', 'sstart_b34', 'send_b34',
+                   'sstart_b48', 'send_b48', 'sstart_b1', 'send_b1',
+                   'sstart_b49', 'send_b49', 'sstart_b35', 'send_b35',
+                   'sstart_b410', 'send_b410', 'sstart_b23', 'send_b23',
+                   'sstart_b411', 'send_b411', 'sstart_b36', 'send_b36',
+                   'sstart_b412', 'send_b412', 'sstart_b12', 'send_b12',
+                   'sstart_b413', 'send_b413', 'sstart_b37', 'send_b37',
+                   'sstart_b414', 'send_b414', 'sstart_b24', 'send_b24',
+                   'sstart_b415', 'send_b415', 'sstart_b38', 'send_b38',
+                   'sstart_b416', 'send_b416']
+
+df = df[desired_columns]
+
+# 遍历DataFrame的每个单元格，应用处理函数
+df = df.applymap(process_cell)
+
+# 将新的DataFrame保存到新的CSV文件中
+output_file = 'urnew.csv'
+df.to_csv(output_file, sep='\t', index=False)  # 保存为制表符分隔的文件
---
+++ Analysis/02/Subgenome.r
@@ -0,0 +1,153 @@
+fmt <- read.csv("format.csv")
+S5 <- fmt$S5  #{5ITR_S}  
+E5 <- fmt$E5  #{5ITR_E}   
+M1 <- fmt$M1  #{5ITR_E + 1}
+M2 <- fmt$M2  #{3ITR_S - 1}  
+S3 <- fmt$S3  #{3ITR_S}     
+E3 <- fmt$E3  #{3ITR_E}    
+SP <- fmt$SP  #{P_S}, promoter 5
+EP <- fmt$EP  #{P_E}, promoter 3
+
+Subgenome <- function(u){
+
+# TYPE 1: full length
+pfull <- subset(u,u[2]<= E5 & u[2]>= S5 & u[3]<= E3 & u[3]>= S3 & is.na(u[4]))
+mfull <- subset(u,u[2]<= E3 & u[2]>= S3 & u[3]<= E5 & u[3]>= S5 & is.na(u[4]))
+full_1st <- rbind(pfull,mfull)
+full_1st[is.na(full_1st)] <- ""
+write.csv(full_1st,"full_1st.csv",col.names=T, row.names=F)
+
+pfullnd <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]<=E3 & u[3]>=S3 & u[4]<=E3 & u[4]>=S3 & u[5]<=E5 & u[5]>=S5 & is.na(u[6]))
+mfullnd <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]<=E5 & u[3]>=S5 & u[4]<=E5 & u[4]>=S5 & u[5]<=E3 & u[5]>=S3 & is.na(u[6]))
+full_2nd <- rbind(pfullnd,mfullnd)
+full_2nd[is.na(full_2nd)] <- ""
+write.csv(full_2nd,"full_2nd.csv",col.names=T, row.names=F)
+
+# TYPE 2: Snapback genome (SBG)<=M2 & u[3]>=EP & u[4]<=M2 & u[4]>=EP & u[5]<=E5 & u[5]>=S5 & is.na(u[6]))
+SBG5 <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]<=M2 & u[3]>=M1 & u[4]<=M2 & u[4]>=M1 & u[5]<=E5 & u[5]>=S5 & is.na(u[6]))
+SBG3 <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]<=M2 & u[3]>=M1 & u[4]<=M2 & u[4]>=M1 & u[5]<=E3 & u[5]>=S3 & is.na(u[6]))
+SBG_1st <- rbind(SBG5,SBG3)
+SBG_1st[is.na(SBG_1st)] <- ""
+write.csv(SBG_1st,"SBG_1st.csv",col.names=T, row.names=F)
+
+#(optional)
+SBG5_p <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]<=M2 & u[3]>=EP & u[4]<=M2 & u[4]>=EP & u[5]<=E5 & u[5]>=S5 & is.na(u[6]))
+SBG5_p[is.na(SBG5_p)] <- ""
+write.csv(SBG5_p,"SBG5_p.csv",col.names=T, row.names=F)
+
+#2nd
+SBG5nd <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]>=M1 & u[3]<=M2& u[4]>=M1 & u[4]<=M2 & u[5]<=E5 & u[5]>=S5 & u[6]<=E5 & u[6]>=S5 & u[7]>=M1 & u[7]<=M2 & u[8]>=M1 & u[8]<=M2 & u[9]<=E5 & u[9]>=S5 & is.na(u[10]))
+SBG3nd <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]>=M1 & u[3]<=M2& u[4]>=M1 & u[4]<=M2 & u[5]<=E3 & u[5]>=S3 & u[6]<=E3 & u[6]>=S3 & u[7]>=M1 & u[7]<=M2 & u[8]>=M1 & u[8]<=M2 & u[9]<=E3 & u[9]>=S3 & is.na(u[10]))
+SBG_2nd <- rbind(SBG5nd,SBG3nd)
+SBG_2nd[is.na(SBG_2nd)] <- ""
+write.csv(SBG_2nd,"SBG_2nd.csv",col.names=T, row.names=F)
+
+#(optional)
+SBG5nd_p <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]>=EP & u[3]<=M2& u[4]>=EP & u[4]<=M2 & u[5]<=E5& u[5]>= S5 & u[6]<=E5& u[6]>=S5 & u[7]>=EP & u[7]<=M2 & u[8]>=EP & u[8]<=M2 & u[9]<=E5& u[9]>=S5 & is.na(u[10]))
+SBG5nd_p[is.na(SBG5nd_p)] <- ""
+write.csv(SBG5nd_p,"SBG5nd_p.csv",col.names=T, row.names=F)
+
+# TYPE 3: Incomplete genome (ICG)
+pICG5 <- subset(u,u[2]<=E5 & u[2]>=S5 & u[3]>=M1 & is.na(u[4]))
+mICG5 <- subset(u,u[2]<=M2 & u[2]>=M1 & u[3]<=E5& u[3]>=S5 & is.na(u[4]))
+pICG3 <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]>=M1 & is.na(u[4]))
+mICG3 <- subset(u,u[2]<=M2 & u[2]>=M1 & u[3]<=E3 & u[3]>=S3  & is.na(u[4]))
+ICG_1st <- rbind(pICG5,mICG5,pICG3,mICG3)
+ICG_1st[is.na(ICG_1st)] <- ""
+write.csv(ICG_1st,"ICG_1st.csv",col.names=T, row.names=F)
+
+ICG5nd <- subset(u,u[2]<=M2 & u[2]>=M1 & u[3]<=E5& u[3]>=S5 & u[4]<=E5& u[4]>=S5 & u[5]<=M2 & u[5]>=M1 & is.na(u[6]))
+ICG3nd  <- subset(u,u[2]<=M2 & u[2]>=M1 & u[3]<=E3 & u[3]>=S3 & u[4]<=E3 & u[4]>=S3 & u[5]<=M2 & u[5]>=M1 & is.na(u[6]))
+ICG_2nd <- rbind(ICG5nd,ICG3nd)
+ICG_2nd[is.na(ICG_2nd)] <- ""
+write.csv(ICG_2nd,"ICG_2nd.csv",col.names=T, row.names=F)
+
+# TYPE 4: Genome deletion mutute (GDM)
+pGDM <- subset(u,u[2]<=E5& u[2]>=S5 & u[3]>=M1 & u[3]<=M2 & u[4]>=M1 & u[4]<=M2 & u[5]<=E3 & u[5]>= S3 & is.na(u[6]))
+mGDM <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]>=M1 & u[3]<=M2 & u[4]>=M1 & u[4]<=M2 & u[5]<= E5& u[5]>= S5 & is.na(u[6]))
+GDM_1st <- rbind(pGDM,mGDM)
+GDM_1st[is.na(GDM_1st)] <- ""
+write.csv(GDM_1st,"GDM_1st.csv",col.names=T, row.names=F)
+
+mGDMnd <- subset(u,u[2]<=E5& u[2]>=S5 & u[3]>=M1 & u[3]<=M2& u[4]>=M1 & u[4]<=M2 & u[5]<=E3 & u[5]>=S3 & u[6]<=E3 & u[6]>=S3 & u[7]>=M1 & u[7]<=M2 & u[8]>=M1 & u[8]<=M2 & u[9]<=E5& u[9]>=S5 & is.na(u[10]))
+pGDMnd <- subset(u,u[2]<=E3 & u[2]>=S3 & u[3]>=M1 & u[3]<=M2& u[4]>=M1 & u[4]<=M2 & u[5]<=E5& u[5]>=S5 & u[6]<=E5 & u[6]>=S5 & u[7]>=M1 & u[7]<=M2 & u[8]>=M1 & u[8]<=M2 & u[9]<=E3 & u[9]>=S3 & is.na(u[10]))
+GDM_2nd <- rbind(pGDMnd,mGDMnd)
+GDM_2nd[is.na(GDM_2nd)] <- ""
+write.csv(GDM_2nd,"GDM_2nd.csv",col.names=T, row.names=F)
+
+## Show the subgenomic number in the rAAV population. 
+##
+full <- (dim(full_1st)+dim(full_2nd))
+nfull <- full[1]
+rfull <- nfull/dim(u)[1]*100
+
+SBG <- (dim(SBG_1st)+dim(SBG_2nd))
+nSBG <- SBG[1]
+rSBG <- nSBG/dim(u)[1]*100
+
+a5SBG <- (dim(SBG5)+dim(SBG5nd))
+n5SBG <- a5SBG[1]
+r5SBG <- n5SBG/dim(u)[1]*100
+
+a3SBG <- (dim(SBG3)+dim(SBG3nd))
+n3SBG <- a3SBG[1]
+r3SBG <- n3SBG/dim(u)[1]*100
+
+SBG5p <- (dim(SBG5_p)+dim(SBG5nd_p))
+nSBG5p <- SBG5p[1]
+rSBG5p <- nSBG5p/dim(u)[1]*100
+
+ICG <- (dim(ICG_1st)+dim(ICG_2nd))
+nICG <- ICG[1]
+rICG <- nICG/dim(u)[1]*100
+
+GDM <- (dim(GDM_1st)+dim(GDM_2nd))
+nGDM <- GDM[1]
+rGDM <- nGDM/dim(u)[1]*100
+
+SUM <- (dim(full_1st)+dim(full_2nd)+dim(SBG_1st)+dim(SBG_2nd)+dim(ICG_1st)+dim(ICG_2nd)+dim(GDM_1st)+dim(GDM_2nd))
+nSUM <- SUM[1]
+rSUM <- nSUM/dim(u)[1]*100
+n <- dim(u)[1]
+
+
+## None SBGp
+# TYPE <- c("SUM","FULL","SBG","GDM","ICG")
+# SUM <- c(nSUM,nfull,nSBG,nGDM,nICG)
+# RATIO <- c(rSUM,rfull,rSBG,rGDM,rICG)
+# x = data.frame(SUM,RATIO)
+# namer <- basename(getwd())
+# colnames(x) <- c(paste0(namer,"_NUMS"),paste0(namer))
+# rownames(x) <- c("SUM","FULL","SBG","GDM","ICG")
+# write.csv(x,paste0(namer,"_list.csv"),sep="",row.names=T,col.names=T)
+
+SUM <- c(nSUM,nfull,nGDM,nICG,nSBG,n5SBG,n3SBG,nSBG5p)
+RATIO <- c(rSUM,rfull,rGDM,rICG,rSBG,r5SBG,r3SBG,rSBG5p)
+x = data.frame(SUM,RATIO)
+namer <- basename(getwd())
+colnames(x) <- c(paste0(namer,"_NUMS"),paste0(namer))
+rownames(x) <- c("SUM","FULL","GDM","ICG","SBG","5SBG","3SBG","SBG5p")
+write.csv(x,paste0(namer,"_list.csv"),sep="",row.names=T,col.names=T)
+}
+
+## Run Subgenome function in specific size range.
+## (i) ALL Aligned Reads
+u <- read.csv("u.csv")
+len <- read.csv("Flen.txt", sep="\t")
+Subgenome(u)
+
+## (ii) Subsample based on size and caculate the most ratio of HiFi data. Size filtering: target <- subset(u,{size filter setting})
+# ## 0-1k bp
+# setwd(patht)  
+# u <- read.csv("u.csv")
+# len <- read.csv("Flen.txt", sep="\t")
+# setwd(pathf)
+# dir.create("k01")
+# setwd(path.expand("./k01"))
+# k01 <- subset(len,len[2] <= 1000 )
+# k01 <- subset(u, u$id %in% k01$X.id)
+# k01[is.na(k01)] <- ""
+# write.csv(k01,"k01.csv",col.names=T, row.names=F)
+# u <- read.csv("k01.csv")
+# n01 <- dim(u)
+# Subgenome(u)
