4b6a0c1c9ca1b03b9ce7c6a108fb15853d7500b5
Sophia_Yu <35753770+xiangpingyu@users.noreply.github.com>
2023-11-25 20:29:54 +0800

    Update 02Rearrange.sh

--- 02Rearrange.sh
+++ 02Rearrange.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-## [2] Rearrange {b*}  
-python Rearrange1_sorted_qcom.py && python Rearrange2_rearrange_qnew.py &&  python Rearrange31_qnew_reordered_q.py &&  python Rearrange32_qnew_reordered_s.py
+# Ensure the script stops on any error
+set -e
 
-### "Delimiters: Space" uqnew.csv and urnew.csv，and then run
+# Notify the start of script execution
+echo "Starting the rearrangement process..."
 
+# [2] Rearrange {b*}
+echo "Running Python scripts for rearrangement..."
+python Rearrange1_sorted_qcom.py 
+python Rearrange2_rearrange_qnew.py 
+python Rearrange31_qnew_reordered_q.py
+python Rearrange32_qnew_reordered_s.py
+
+# Check if the necessary files exist
+if [[ ! -f "export/t/urnew.csv" ]] || [[ ! -f "export/t/uqnew.csv" ]]; then
+    echo "Error: Required files do not exist."
+    exit 1
+fi
+
+# Transform urnew.csv and uqnew.csv into a space-delimited format
+echo "Transforming urnew.csv and uqnew.csv file formats..."
+awk '{for (i=1; i<=NF; i++) printf "%s%s", $i, (i==NF ? "\n" : ",")} ' export/t/urnew.csv > export/t/urnew_modified.csv
+awk '{for (i=1; i<=NF; i++) printf "%s%s", $i, (i==NF ? "\n" : ",")} ' export/t/uqnew.csv > export/t/uqnew_modified.csv
+
+# Continue with further Python processing
+echo "Merging and further processing data..."
 python Rearrange4_qs_merged_u.py
+
+# Notify the script execution completion
+echo "Script execution completed."
+
