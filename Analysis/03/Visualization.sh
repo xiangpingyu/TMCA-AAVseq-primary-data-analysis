c045cd74dcdbfe8b7d85c1fcfaf97546e29adfcf
Sophia_Yu <35753770+xiangpingyu@users.noreply.github.com>
2023-11-25 18:46:36 +0800

    Delete Analysis directory

--- Analysis/03/Visualization.sh
+++
@@ -1,7 +0,0 @@
-## Visualization {b*} alignments:
-
-##  Update genome format information.
-### Edit "format.csv", the important sites of rAAV genome, e.g. ITR and promoter, refering to your reference genome.
-### Place Flen.txt in this directory
-
-Rscript Subgenome.r 
