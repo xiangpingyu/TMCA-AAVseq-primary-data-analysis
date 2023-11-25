f93182577417293ac00521eef488f45562cdc428
Sophia_Yu <35753770+xiangpingyu@users.noreply.github.com>
2023-11-25 19:51:26 +0800

    Update 02Rearrange.sh

--- 02Rearrange.sh
+++ 02Rearrange.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ## [2] Rearrange {b*}  
-python Rearrange1_sorted_qcom.py && python Rearrange2_rearrange_qnew.py &&  python Rearrange31_qnew_reordered_q.py &&  python Rearrange32_qnew_reordered_s
+python Rearrange1_sorted_qcom.py && python Rearrange2_rearrange_qnew.py &&  python Rearrange31_qnew_reordered_q.py &&  python Rearrange32_qnew_reordered_s.py
 
 ### "Delimiters: Space" uqnew.csv and urnew.csv，and then run
-echo "Hello"
 
 python Rearrange4_qs_merged_u
